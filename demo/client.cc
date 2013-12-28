527c73b16df874d1bb5c787ffd3dad777e108264
ASMlover <asmlover@126.com>
2013-12-28 23:30:39 +0800

    modified server and client demo for elibpp network module

--- demo/client.cc
+++ demo/client.cc
@@ -24,10 +24,7 @@
 //! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 //! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 //! POSSIBILITY OF SUCH DAMAGE.
-#include "../el_net_internal.h"
-#include "../el_time.h"
-#include "../el_net.h"
-#include "../el_socket.h"
+#include "../elibpp_net.h"
 
 
 
