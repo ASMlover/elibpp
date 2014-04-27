100999524bd2b50b21b23475728230d1c8f635be
ASMlover <asmlover@126.com>
2014-04-27 21:43:09 +0800

    fixed bug of Allocator

--- el_allocator.cc
+++ el_allocator.cc
@@ -1,29 +1,29 @@
-//! Copyright (c) 2013 ASMlover. All rights reserved.
-//!
-//! Redistribution and use in source and binary forms, with or without
-//! modification, are permitted provided that the following conditions
-//! are met:
-//!
-//!  * Redistributions of source code must retain the above copyright
-//!    notice, this list ofconditions and the following disclaimer.
-//!
-//!  * Redistributions in binary form must reproduce the above copyright
-//!    notice, this list of conditions and the following disclaimer in
-//!    the documentation and/or other materialsprovided with the
-//!    distribution.
-//!
-//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//! POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2013 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 #include "elib_internal.h"
 #include "el_allocator.h"
 
@@ -38,9 +38,7 @@ struct Memory {
 
 
 
-Memory* 
-Allocator::AllocChunk(size_t index)
-{
+Memory* Allocator::AllocChunk(size_t index) {
   size_t alloc_size = (index + 1) * ALIGN + PREFIX_SIZE;
   size_t chunk_size = alloc_size * MAX_NUMBER;
 
@@ -61,9 +59,7 @@ Allocator::AllocChunk(size_t index)
   return free_list_[index];
 }
 
-void 
-Allocator::InsertChunk(void* chunk)
-{
+void Allocator::InsertChunk(void* chunk) {
   if (chunk_count_ == chunk_storage_) {
     size_t new_chunk_storage = chunk_storage_ + NFREELISTS;
     void** new_chunk_list = 
@@ -84,32 +80,23 @@ Allocator::InsertChunk(void* chunk)
 
 
 
-Allocator::Allocator(void)
-{
-  memset(free_list_, 0, sizeof(chunk_list_));
+Allocator::Allocator(void) {
+  memset(free_list_, 0, sizeof(free_list_));
   chunk_list_ = (void**)malloc(sizeof(void*) * NFREELISTS);
   assert(NULL != chunk_list_);
   chunk_count_ = 0;
   chunk_storage_ = NFREELISTS;
-
-  spinlock_ = new SpinLock();
-  assert(NULL != spinlock_);
 }
 
-Allocator::~Allocator(void)
-{
+Allocator::~Allocator(void) {
   for (size_t i = 0; i < chunk_count_; ++i)
     free(chunk_list_[i]);
   free(chunk_list_);
-
-  delete spinlock_;
 }
 
-void* 
-Allocator::Malloc(size_t bytes)
-{
-  //! Allocator must has beed initialized 
-  //! and bytes must > 0
+void* Allocator::Malloc(size_t bytes) {
+  // Allocator must has beed initialized 
+  // and bytes must > 0
 
   assert(bytes > 0);
 
@@ -122,7 +109,7 @@ Allocator::Malloc(size_t bytes)
   else {
     size_t index = FreeListIndex(bytes);
 
-    SpinLockGuard lock(*spinlock_);
+    LockerGuard<SpinLock> guard(locker_);
     if (NULL == free_list_[index])
       AllocChunk(index);
 
@@ -133,9 +120,7 @@ Allocator::Malloc(size_t bytes)
   return ret;
 }
 
-void 
-Allocator::Free(void* ptr)
-{
+void Allocator::Free(void* ptr) {
   assert(NULL != ptr);
 
   void* realptr = (uint8_t*)ptr - PREFIX_SIZE;
@@ -146,7 +131,7 @@ Allocator::Free(void* ptr)
   else if (index < NFREELISTS) {
     Memory* free_block = (Memory*)realptr;
 
-    SpinLockGuard lock(*spinlock_);
+    LockerGuard<SpinLock> guard(locker_);
     free_block->next = free_list_[index];
     free_list_[index] = free_block;
   }
