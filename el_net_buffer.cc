03fca73baa577bd0ac75fc48f728273d38e6d317
ASMlover <asmlover@126.com>
2014-04-29 21:41:23 +0800

    updated net buffer module for elibpp

--- el_net_buffer.cc
+++ el_net_buffer.cc
@@ -1,29 +1,29 @@
-//! Copyright (c) 2013 ASMlover. All rights reserved.
-//!
-//! Redistribution and use in source and binary forms, with or without
-//! modification, are permitted provided that the following conditions
-//! are met:
-//!
-//!  * Redistributions of source code must retain the above copyright
-//!    notice, this list ofconditions and the following disclaimer.
-//!
-//!  * Redistributions in binary form must reproduce the above copyright
-//!    notice, this list of conditions and the following disclaimer in
-//!    the documentation and/or other materialsprovided with the
-//!    distribution.
-//!
-//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//! POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2013 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 #include "el_net_internal.h"
 #include "el_net_buffer.h"
 
@@ -34,44 +34,36 @@ namespace el {
 
 
 NetBuffer::NetBuffer(void)
-  : buffer_(NULL)
+  : buffer_(nullptr)
   , length_(0)
-  , storage_(kDefaultStorage)
-{
+  , storage_(DEF_STORAGE) {
 }
 
-NetBuffer::~NetBuffer(void)
-{
+NetBuffer::~NetBuffer(void) {
   Destroy();
 }
 
-bool 
-NetBuffer::Init(int storage)
-{
-  storage_ = (storage > kDefaultStorage ? storage : kDefaultStorage);
+bool NetBuffer::Init(int storage) {
+  storage_ = (storage > DEF_STORAGE ? storage : DEF_STORAGE);
 
   length_ = 0;
   buffer_ = (char*)malloc(storage_);
-  if (NULL == buffer_)
+  if (nullptr == buffer_)
     return false;
 
   return true;
 }
 
-void 
-NetBuffer::Destroy(void)
-{
-  if (NULL != buffer_) {
+void NetBuffer::Destroy(void) {
+  if (nullptr != buffer_) {
     free(buffer_);
-    buffer_ = NULL;
+    buffer_ = nullptr;
   }
   length_ = 0;
-  storage_ = kDefaultStorage;
+  storage_ = DEF_STORAGE;
 }
 
-int 
-NetBuffer::Put(const char* buffer, int bytes)
-{
+int NetBuffer::Put(const char* buffer, int bytes) {
   if (length_ + bytes > storage_) {
     if (!Regrow())
       return 0;
@@ -83,9 +75,7 @@ NetBuffer::Put(const char* buffer, int bytes)
   return bytes;
 }
 
-int 
-NetBuffer::Get(int bytes, char* buffer)
-{
+int NetBuffer::Get(int bytes, char* buffer) {
   int copy_bytes = length_ <= bytes ? length_ : bytes;
   if (0 == copy_bytes)
     return 0;
@@ -98,9 +88,7 @@ NetBuffer::Get(int bytes, char* buffer)
   return copy_bytes;
 }
 
-int 
-NetBuffer::Increment(int bytes)
-{
+int NetBuffer::Increment(int bytes) {
   if (length_ + bytes > storage_) {
     if (!Regrow())
       return 0;
@@ -110,9 +98,7 @@ NetBuffer::Increment(int bytes)
   return bytes;
 }
 
-int 
-NetBuffer::Decrement(int bytes)
-{
+int NetBuffer::Decrement(int bytes) {
   if (bytes > length_) {
     abort();
     return -1;
@@ -125,12 +111,10 @@ NetBuffer::Decrement(int bytes)
   return bytes;
 }
 
-bool 
-NetBuffer::Regrow(void)
-{
-  int new_storage = (0 != storage_ ? 2 * storage_ : kDefaultStorage);
+bool NetBuffer::Regrow(void) {
+  int new_storage = (0 != storage_ ? 2 * storage_ : DEF_STORAGE);
   buffer_ = (char*)realloc(buffer_, new_storage);
-  if (NULL == buffer_) {
+  if (nullptr == buffer_) {
     abort();
     return false;
   }
--- el_net_buffer.h
+++ el_net_buffer.h
@@ -1,29 +1,29 @@
-//! Copyright (c) 2013 ASMlover. All rights reserved.
-//!
-//! Redistribution and use in source and binary forms, with or without
-//! modification, are permitted provided that the following conditions
-//! are met:
-//!
-//!  * Redistributions of source code must retain the above copyright
-//!    notice, this list ofconditions and the following disclaimer.
-//!
-//!  * Redistributions in binary form must reproduce the above copyright
-//!    notice, this list of conditions and the following disclaimer in
-//!    the documentation and/or other materialsprovided with the
-//!    distribution.
-//!
-//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//! POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2013 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 #ifndef __EL_NET_BUFFER_HEADER_H__
 #define __EL_NET_BUFFER_HEADER_H__
 
@@ -36,31 +36,27 @@ class NetBuffer : private UnCopyable {
   int   length_;
   int   storage_;
 public:
-  enum { kDefaultStorage = 16 * 1024 };
+  enum { DEF_STORAGE = 16 * 1024 };
   explicit NetBuffer(void);
   ~NetBuffer(void);
 
-  inline const char* buffer(void) const 
-  {
+  inline const char* buffer(void) const {
     return buffer_;
   }
 
-  inline int length(void) const 
-  {
+  inline int length(void) const {
     return length_;
   }
 
-  inline char* free_buffer(void) const 
-  {
+  inline char* free_buffer(void) const {
     return buffer_ + length_;
   }
 
-  inline int free_length(void) const 
-  {
+  inline int free_length(void) const {
     return storage_ - length_;
   }
 public:
-  bool Init(int storage = kDefaultStorage);
+  bool Init(int storage = DEF_STORAGE);
   void Destroy(void);
 
   int Put(const char* buffer, int bytes);
@@ -74,4 +70,4 @@ public:
 
 }
 
-#endif  //! __EL_NET_BUFFER_HEADER_H__
+#endif  // __EL_NET_BUFFER_HEADER_H__
