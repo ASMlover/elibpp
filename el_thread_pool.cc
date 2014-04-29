ed45200a986d4b2e1ed6daf0b63569cfee689abe
ASMlover <asmlover@126.com>
2014-04-29 22:09:49 +0800

    updated the implementation of ThreadPool

--- el_thread_pool.cc
+++ el_thread_pool.cc
@@ -1,29 +1,29 @@
-//! Copyright (c) 2013 ASMlover. All rights reserved.
-//!
-//! Redistribution and use in source and binary forms, with or without
-//! modification, are permitted provided that the following conditions
-//! are met:
-//!
-//!  * Redistributions of source code must retain the above copyright
-//!    notice, this list ofconditions and the following disclaimer.
-//!
-//!  * Redistributions in binary form must reproduce the above copyright
-//!    notice, this list of conditions and the following disclaimer in
-//!    the documentation and/or other materialsprovided with the
-//!    distribution.
-//!
-//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//! POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2013 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 #include "elib_internal.h"
 #include "el_thread_pool.h"
 
@@ -34,23 +34,19 @@ namespace el {
 ThreadPool::ThreadPool(void)
   : mutex_()
   , cond_(mutex_)
-  , running_(false)
-{
+  , running_(false) {
 }
 
-ThreadPool::~ThreadPool(void)
-{
+ThreadPool::~ThreadPool(void) {
   if (running_)
     Stop();
 }
 
-void 
-ThreadPool::Start(int thread_count)
-{
-  if (thread_count < kDefMinThreadsCount)
-    thread_count = kDefMinThreadsCount;
-  if (thread_count > kDefMaxThreadsCount)
-    thread_count = kDefMaxThreadsCount;
+void ThreadPool::Start(int thread_count) {
+  if (thread_count < MIN_THREADS)
+    thread_count = MIN_THREADS;
+  if (thread_count > MAX_THREADS)
+    thread_count = MAX_THREADS;
 
   running_ = true;
   for (int i = 0; i < thread_count; ++i) {
@@ -59,9 +55,7 @@ ThreadPool::Start(int thread_count)
   }
 }
 
-void 
-ThreadPool::Stop(void)
-{
+void ThreadPool::Stop(void) {
   {
     MutexGuard lock(mutex_);
     running_ = false;
@@ -76,9 +70,7 @@ ThreadPool::Stop(void)
   threads_.clear();
 }
 
-void 
-ThreadPool::Run(void (*routine)(void*), void* argument)
-{
+void ThreadPool::Run(void (*routine)(void*), void* argument) {
   if (threads_.empty()) {
     routine(argument);
   }
@@ -91,9 +83,7 @@ ThreadPool::Run(void (*routine)(void*), void* argument)
 }
 
 
-Task 
-ThreadPool::TakeTask(void)
-{
+Task ThreadPool::TakeTask(void) {
   MutexGuard lock(mutex_);
   while (tasks_.empty() && running_)
     cond_.Wait();
@@ -107,9 +97,7 @@ ThreadPool::TakeTask(void)
   return task;
 }
 
-void 
-ThreadPool::Routine(void* argument)
-{
+void ThreadPool::Routine(void* argument) {
   ThreadPool* self = static_cast<ThreadPool*>(argument);
   if (NULL == self)
     abort();
